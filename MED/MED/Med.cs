e7fed0b5108157a3024e9064dc4a5acc7b5cc169
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2014-07-27 18:38:09 +0300

    Rename Form1.cs to Med.cd ...&...

--- MED/MED/Med.cs
+++ MED/MED/Med.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MED
 {
-    public partial class Form1 : Form
+    public partial class Med : Form
     {
-        public Form1()
+        public Med()
         {
             InitializeComponent();
         }
