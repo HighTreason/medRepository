892dcd12596fd092907d3afb4c02beaab90c54af
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2014-07-27 19:08:48 +0300

    add the options class along with the LocationUnit enum ...&...

---
+++ MED/MED/classes/Options.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MED.classes
+{
+    protected class Options
+    {
+        protected enum LocationUnit : int
+        {
+            [Description("None")]
+            None = -1,
+            [Description("Random")]
+            Random = 0,
+            [Description("Street")]
+            Street = 1,
+            [Description("City")]
+            City = 2,
+            [Description("Country")]
+            Country = 3
+        }
+    }
+}
